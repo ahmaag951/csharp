feadf4a26075cb7c175f347597f4aa45f744d01f
ahmaag951 <thisismyway_2011@yahoo.com>
2020-06-05 23:25:37 +0200

    first commit

---
+++ 1. extension methods/test extension method/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test_extension_method.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(){
+        
+            var debug = "qwer".AddABC();
+            return View();
+        }
+
+    }
+
+    public static class StringHandler {
+
+        public static string AddABC(this string source) {
+            return source + "abc";
+        }
+    }
+}
