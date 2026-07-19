cacde0bfba86abb34e8cb434897353c569ed2874
Ahmed E. Alqadi <ahmaag951@gmail.com>
2026-07-19 12:21:16 +0300

    single or default

--- New Samples/4.single or default and first or default/single or default and first or default/single or default and first or default/Program.cs
+++ New Samples/4.single or default and first or default/single or default and first or default/single or default and first or default/Program.cs
@@ -12,7 +12,13 @@
             // this line will throw an exception because there are multiple elements that match the predicate
             // single or default will throw an exception if there are multiple elements that match the predicate
             // we use single or default when we want to gurantee that only one element to match the predicate
-            var singleOrDefault = list.SingleOrDefault(x => x == 1);
+            //var singleOrDefault = list.SingleOrDefault(x => x == 1);
+
+            // how the single and single or default handles the non existing element
+            // single will throw an exception if no element matches the predicate
+            // single or default will return the default value of the type if no element matches the predicate
+            var singleOrDefaultNonExisting = list.SingleOrDefault(x => x == 6);
+            var singleNonExisting = list.Single(x => x == 6);
         }
     }
 }
